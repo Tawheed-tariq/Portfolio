f5e21d3ef7fb3119ed254a9bf21e0ad04791a5bb
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-17 16:12:25 +0530

    Added constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,3 +58,156 @@ export const experience = [
         end: "Present"
     }
 ]
+
+export const education = [
+    {
+        id: "1",
+        icon : "NIT.png",
+        title: "National Institute of technology Srinagar",
+        desc: "Currently pursuing B-tech in Information Technology from National Institute of technology Srinagar, I have completed 3 semesters and have a CGPA of 9, I have taken courses in Data structures, Object Oriented Programming in C++ and others.",
+        strt: "Nov 2022",
+        end: "Present",
+        grade: "9 CGPA"
+    },
+    {
+        id: "2",
+        icon : "school.png",
+        title: "MPML Higher secondary",
+        desc: "Completed my Higher secondary from MPML Higher Secondary School, Srinagar.",
+        strt: "Dec 2019",
+        end: "Dec 2021",
+        grade: "98.2 %"
+    },
+]
+
+export const frontend_skills = [
+    {
+        id: "1",
+        icon: "html.png",
+        title: "HTML"
+    },
+    {
+        id: "2",
+        icon: "css.png",
+        title: "CSS"
+    },
+    {
+        id: "3",
+        icon: "js.png",
+        title: "JavaScript"
+    },
+    {
+        id: "4",
+        icon: "tailwind css.png",
+        title: "Tailwind CSS"
+    },
+    {
+        id: "5",
+        icon: "reactjs.png",
+        title: "React.JS"
+    }
+]
+
+
+export const backend_skills = [
+    {
+        id: "1",
+        icon: "c.png",
+        title: "C Programming"
+    },
+    {
+        id: "2",
+        icon: "c++.png",
+        title: "C++"
+    },
+    {
+        id: "3",
+        icon: "nodejs.png",
+        title: "Node.JS"
+    },
+    {
+        id: "4",
+        icon: "expressjs.png",
+        title: "Express.JS"
+    },
+    {
+        id: "5",
+        icon: "mongodb.png",
+        title: "MongoDB"
+    },
+    {
+        id: "6",
+        icon: "python.png",
+        title: "Python"
+    },
+    {
+        id: "7",
+        icon: "firebase.png",
+        title: "Firebase"
+    }
+]
+
+export const tools = [
+    {
+        id: "1",
+        icon: "linux.png",
+        title: "Linux"
+    },
+    {
+        id: "2",
+        icon: "git.png",
+        title: "Git"
+    },
+    {
+        id: "3",
+        icon: "github.png",
+        title: "GitHub"
+    },
+    {
+        id: "4",
+        icon: "vscode.png",
+        title: "VScode"
+    },
+    {
+        id: "5",
+        icon: "netlify.png",
+        title: "Netlify"
+    },
+    {
+        id: "6",
+        icon: "figma.png",
+        title: "Figma"
+    }
+]
+
+
+export const projects = [
+    {
+        id: "1",
+        title : "Car Racing Game",
+        desc: "A car racing game which shows the score gained, highest score and on crash resets the game.",
+        skills_used: "HTML, CSS, JavaScript",
+        img: ""
+    },
+    {
+        id: "2",
+        title : "Crypto Dashboard",
+        desc: "A crypto currency Dashboard , uses crypto api to get the data of crypto currencies",
+        skills_used: "HTML, CSS, JavaScript, React.js. ChakraUI, Figma.",
+        img: ""
+    },
+    {
+        id: "3",
+        title : "Chat App",
+        desc: "A real-time chat application which can be used to chat with our friends in real-time.",
+        skills_used: "HTML, CSS, JavaScript, React.js, ChakraUI,Node.js, Express.js, MongoDB, Figma.",
+        img: ""
+    },
+    {
+        id: "4",
+        title : "Google maps Scraper",
+        desc: "A web-scraper which scrapes google maps based on the search and provides an excel file of output which contains name, address, website url, phone number and reviews of the given search.",
+        skills_used: " Python, Pandas, argparse. playwright, web-scraping",
+        img: ""
+    },
+]
