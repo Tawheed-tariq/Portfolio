193abca8bbb348b77a4b5bd068220febccbd1b57
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-20 15:22:38 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -151,23 +151,6 @@ export const backend_skills = [
     }
 ]
 
-export const skills = [
-    {
-        id: "1",
-        name: "Frontend",
-        arrName: frontend_skills
-    },
-    {
-        id: "2",
-        name: "Backend",
-        arrName: backend_skills
-    },
-    {
-        id: "3",
-        name: "Tools",
-        arrName: tools
-    }
-]
 
 export const tools = [
     {
@@ -199,6 +182,24 @@ export const tools = [
         id: "6",
         icon: "figma.png",
         title: "Figma"
+    }
+]
+
+export const skills = [
+    {
+        id: "1",
+        name: "Frontend",
+        arrName: frontend_skills
+    },
+    {
+        id: "2",
+        name: "Backend",
+        arrName: backend_skills
+    },
+    {
+        id: "3",
+        name: "Tools",
+        arrName: tools
     }
 ]
 
