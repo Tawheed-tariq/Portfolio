a8b9febfa8d5c404060835591d6ba1839d168904
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-03-21 20:16:47 +0530

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import {projects} from '../constants/index'
 import Project from './project'
 export default function Projects(){
     return(
-        <div id="project" className={`w-full flex flex-col justify-center px-[20px] md:px-[40px] lg:px-[150px]`}>
+        <div id="project" className={`w-full flex flex-col justify-center px-[20px] lg:px-[150px]`}>
             <h1 className={`text-2xl text-center mb-[20px]  font-bold md:text-3xl`}><span className="heading">Projects</span>  </h1>
             <div className='w-full flex flex-wrap justify-center'>
                 {
