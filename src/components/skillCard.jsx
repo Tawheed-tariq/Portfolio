62dc60eaea332dd7176d1dce1bdd94a0596ed855
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-08-07 11:07:10 +0530

    commit

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -7,9 +7,9 @@ import "aos/dist/aos.css"
 
 
 export default function SkillCard({skillName, skillArray}){
-    useEffect(()=>{
-        AOS.init({duration:900, anchorPlacement:"top-center"}),[]
-    })
+    // useEffect(()=>{
+    //     AOS.init({duration:600, anchorPlacement:"top-center"}),[]
+    // })
     return(
         <div data-aos="zoom-in-up" className={`border-[1px] sm:w-[700px] md:w-[900px] border-gray-400 px-[25px] py-[10px] my-[20px] rounded-3xl`}>
             <h1 className={`text-xl md:text-2xl font-bold mb-[10px]`}><span className="decorate-text">{skillName}</span></h1>
